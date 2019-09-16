ebb1b4e72c14b4c4f647c907c04cb71213cf5c19
abdonaser10 <40724937+abdonaser10@users.noreply.github.com>
2019-09-16 16:56:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,12 +65,12 @@ client.on('message', message => {
     var Music = client.emojis.find(emoji => emoji.name === "Music" )
     message.channel.send(`${yes1} **| قائمة الأوامر
 ╔════════════════════════╗
+║${yes1} | \`${prefix}inv\` => لـدعوت البوت الي سيرفر. ${yes1}
 ║${yes1} | \`${prefix}bc\` => لأرسال برود كاست للأونلاين. ${yes1}     
 ║${yes1} | \`${prefix}avatar\` => لعرض الصورة حقك. ${yes1}
 ║${yes1} | \`${prefix}bot\` => لعرض معلومات البوت. ${yes1}
 ║${yes1} | \`${prefix}mc\` => يقفل لك الشات. ${yes1}
 ║${yes1} | \`${prefix}uc\` => يفتح لك الشات. ${yes1}
-║${yes1} | \`${prefix}inv\` => لـدعوت البوت الي سيرفر. ${yes1}
 ╚════════════════════════╝**`)
   }
 });
